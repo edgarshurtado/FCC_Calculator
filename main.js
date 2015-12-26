1da00f55f7597237a3d59a20f65a6b5b59af78ca
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-26 11:20:48 +0100

    Implement enter sucessive numbers
    Check if the current number is 0 and that the introduced number is
    shorter than 15 characters long

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var c = document.getElementById("c");
 
 $(".numeric_button").click(function(){
-    $('#displayNumbers').text($(this).text());
+    var currentText = $('#displayNumbers').text();
+    if(currentText == 0){
+        $('#displayNumbers').text($(this).text());
+    }else if(currentText.length < 15){
+        $('#displayNumbers').text(currentText + $(this).text());
+    }
 });
