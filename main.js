45732082f441ade791574c510bd11ea1aa4e6e87
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-26 11:35:02 +0100

    Implement display numbers deletion

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var c = document.getElementById("c");
-
 // Introduce numbers by clicking
 $(".numeric_button").click(function(){
     addNumberToDisplay($(this).text());
 });
 
+// Introducing numbers to the display control
 function addNumberToDisplay(number) {
     var currentText = $('#displayNumbers').text();
     if(currentText == 0){
@@ -12,4 +11,12 @@ function addNumberToDisplay(number) {
     }else if(currentText.length < 15){
         $('#displayNumbers').text(currentText + number);
     }
+}
+
+// -- Delete current number in display --
+$('#c').click(deleteCurrentNumber);
+
+// Controller
+function deleteCurrentNumber(){
+    $('#displayNumbers').text('0');
 }
