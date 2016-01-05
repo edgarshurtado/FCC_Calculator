c4ba6dcfebd747878477c88e4bc8d700b0472112
Edgar S. Hurtado <edsanhu@gmail.com>
2016-01-05 17:57:19 +0100

    Add classic calculator performance to addition
    The calculator is available to make additions. Furthermore, it's been
    implemented that the firs operator number remains in the screen after
    clicking and operational button since another number is clicked.
    
    Created the basic switch skeleton for the remaining operational buttons

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ $(".numeric_button").click(function(){
 // Introducing numbers to the display control
 function addNumberToDisplay(number) {
     var currentText = $('#number').text();
-    if(currentText === '0'){
+    if(currentText === '0' || calcObject.lastButton.indexOf('operational_button') !== -1){
         $('#number').text(number);
     }else if(currentText.length < 15){
         $('#number').text(currentText + number);
     }
+
+    calcObject.lastButton = $(this).attr('class');
 }
 
 // -- Delete current number in display --
@@ -31,4 +33,65 @@ $("#change_sign").click(function(){
     }else if(currentSign === "-"){
         $('#sign').text(""); 
     }   
+});
+
+// Operational_buttons
+$('.operational_button').click(function(){
+    var operationSymbol = $(this).text();
+    calcObject.setOperand(1, parseInt($('#number').text(), 10));
+
+    switch (operationSymbol) {
+        case '+':
+            calcObject.setActiveOperator('addition');
+            break;
+        case '%':
+            calcObject.setActiveOperator('division');
+            break;
+        case '-':
+            calcObject.setActiveOperator('substraction');
+            break;
+        case '√':
+            calcObject.setActiveOperator('sqrt');
+            break;
+        case '*':
+            calcObject.setActiveOperator('multiplication');
+            break;
+        case '/':
+            calcObject.setActiveOperator('division');
+            break;
+        default:
+            
+    }
+    calcObject.lastButton = $(this).attr('class');
+});
+
+$('#equal_button').click(function(){
+    calcObject.setOperand(2, parseInt($('#number').text(), 10));
+    var solution = calcObject.solve();
+    $('#number').text(solution);
 })
+
+// ---- CALC LOGIC ----
+
+var calcObject = {
+    operand1 : null,
+    operand2 : null,
+    activeOperator : null,
+    lastButton: null,
+
+    setOperand: function(operandNumber, number){
+        this["operand" + operandNumber] = number;
+    },
+    
+    setActiveOperator: function(operatorSymbol){
+        this.activeOperator = operatorSymbol;
+    },
+
+    addition : function(){
+        return this.operand1 + this.operand2;
+    },
+
+    solve: function(){
+        return this[this.activeOperator]();
+    }
+}
